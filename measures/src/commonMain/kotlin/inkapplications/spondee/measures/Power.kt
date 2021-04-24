f1d6f9bf623592f8357ce0da12f9ca5f06534bb2
Renee Vandervelde <Renee@ReneeVandervelde.com>
2021-04-24 07:54:07 -0500

    Add operators to power unit

--- measures/src/commonMain/kotlin/inkapplications/spondee/measures/Power.kt
+++ measures/src/commonMain/kotlin/inkapplications/spondee/measures/Power.kt
@@ -8,12 +8,27 @@ interface Power {
      * Express the power in watts.
      */
     val inWatts: Double
+
+    operator fun compareTo(other: Power): Int
+    operator fun plus(other: Power): Power
+    operator fun minus(other: Power): Power
+    operator fun div(other: Number): Power
+    operator fun times(other: Number): Power
+    operator fun unaryPlus(): Power = this
+    operator fun unaryMinus(): Power
 }
 
 /**
  * SI Unit of Power.
  */
 internal data class Watts(override val inWatts: Double): Power {
+    override fun compareTo(other: Power) = inWatts.compareTo(other.inWatts)
+    override fun plus(other: Power): Power = Watts(inWatts + other.inWatts)
+    override fun minus(other: Power): Power = Watts(inWatts - other.inWatts)
+    override fun div(other: Number): Power = Watts(inWatts / other.toDouble())
+    override fun times(other: Number): Power = Watts(inWatts * other.toDouble())
+    override fun unaryMinus(): Power = Watts(-inWatts)
+
     override fun toString(): String = "${inWatts}W"
 }
 
