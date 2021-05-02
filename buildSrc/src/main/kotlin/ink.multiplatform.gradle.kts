807becc267a7ff25b2abd7b721a5f0cab80f9a5e
Renee Vandervelde <Renee@ReneeVandervelde.com>
2021-05-02 11:11:15 -0500

    Update multiplatform publications and release build

---
+++ buildSrc/src/main/kotlin/ink.multiplatform.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    kotlin("multiplatform")
+}
+
+repositories {
+    mavenCentral()
+0}
+
+kotlin {
+    jvm()
+    js {
+        nodejs()
+        browser()
+    }
+    linuxArm64()
+    linuxArm32Hfp()
+    linuxMips32()
+    linuxX64()
+    mingwX64()
+    macosX64()
+
+    sourceSets {
+        val jvmTest by getting {
+            dependencies {
+                api(kotlin("test"))
+                api(kotlin("test-annotations-common"))
+                api(kotlin("test-junit"))
+            }
+        }
+    }
+}
--- math/build.gradle.kts
+++ math/build.gradle.kts
@@ -1,20 +1,4 @@
 plugins {
-    kotlin("multiplatform")
-    id("maven-publish")
-}
-
-kotlin {
-    jvm()
-    js()
-
-    sourceSets {
-
-        val jvmTest by getting {
-            dependencies {
-                api(kotlin("test"))
-                api(kotlin("test-annotations-common"))
-                api(kotlin("test-junit"))
-            }
-        }
-    }
+    id("ink.multiplatform")
+    id("ink.publishing")
 }
