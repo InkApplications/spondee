4f97f0d62b397cc4e7d065196afa7c2e0e06a12f
Renee Vandervelde <Renee@InkApplications.com>
2021-06-06 16:28:28 -0500

    Add Mass unit

--- measures/src/commonMain/kotlin/inkapplications/spondee/measure/Mass.kt
+++ measures/src/commonMain/kotlin/inkapplications/spondee/measure/Mass.kt
@@ -1,6 +1,7 @@
 package inkapplications.spondee.measure
 
 import inkapplications.spondee.structure.DoubleMeasure
+import inkapplications.spondee.structure.SiValue
 import kotlin.jvm.JvmInline
 
 /**
@@ -10,40 +11,48 @@ interface Mass: DoubleMeasure<Mass> {
     /**
      * Express a mass as a value in grams
      */
-    val inGrams: Double
+    val inGrams: SiValue
 
     /**
      * Express a mass as a value in pounds
      */
-    val inPounds: Double
+    val inPounds: ImperialMass
 }
 
-private const val RATIO = 453.59237
+@JvmInline
+value class ImperialMass(val decimal: Double) {
+    val poundsComponent: Long get() = decimal.toLong()
+    val ouncesComponent: Double get() = ((decimal - poundsComponent) * 16)
+
+    operator fun component1() = poundsComponent
+    operator fun component2() = ouncesComponent
+    override fun toString() = "${poundsComponent}lbs ${ouncesComponent}oz"
+}
+
+private const val GRAMS_PER_POUND = 453.59237
 
 @JvmInline
-internal value class Grams(override val inGrams: Double): Mass {
-    override val inPounds: Double get() = inGrams / RATIO
-    override fun convert(value: Mass): Double = value.inGrams
-    override fun create(value: Double): Mass = Grams(value)
+internal value class Grams(override val inGrams: SiValue): Mass {
+    override val inPounds: ImperialMass get() = ImperialMass(inGrams.baseValue / GRAMS_PER_POUND)
+    override fun convert(value: Mass): Double = value.inGrams.baseValue
+    override fun create(value: Double): Mass = value.grams
     override fun toString(): String = "${inGrams}g"
 }
 
 /**
  * Express a mass in grams
  */
-val Number.grams: Mass get() = Grams(toDouble())
-val gram = 1.grams
+val Number.grams: Mass get() = Grams(SiValue(toDouble()))
 
 @JvmInline
-internal value class Pounds(override val inPounds: Double): Mass {
-    override val inGrams: Double get() = inPounds * RATIO
-    override fun convert(value: Mass): Double = value.inPounds
-    override fun create(value: Double): Mass = Pounds(value)
-    override fun toString(): String = "${inPounds}lbs"
+internal value class Pounds(override val inPounds: ImperialMass): Mass {
+    override val inGrams: SiValue get() = SiValue(inPounds.decimal * GRAMS_PER_POUND)
+    override fun convert(value: Mass): Double = value.inPounds.decimal
+    override fun create(value: Double): Mass = value.pounds
+    override fun toString(): String = inPounds.toString()
 }
 
 /**
  * Express a mass in pounds
  */
-val Number.pounds: Mass get() = Pounds(toDouble())
-val pound = 1.pounds
+val Number.pounds: Mass get() = Pounds(ImperialMass(toDouble()))
--- measures/src/jvmTest/kotlin/inkapplications/spondee/measure/MassTest.kt
+++ measures/src/jvmTest/kotlin/inkapplications/spondee/measure/MassTest.kt
@@ -6,7 +6,9 @@ import kotlin.test.assertEquals
 class MassTest {
     @Test
     fun conversions() {
-        assertEquals(0.27116858249, 123.grams.inPounds, 0.00000000001)
-        assertEquals(55791.86151, 123.pounds.inGrams, 0.000001)
+        assertEquals(0.27116858249, 123.grams.inPounds.decimal, 0.00000000001)
+        assertEquals(55791.86151, 123.pounds.inGrams.baseValue, 0.000001)
+        assertEquals(2, 2.5.pounds.inPounds.poundsComponent)
+        assertEquals(8.0, 2.5.pounds.inPounds.ouncesComponent)
     }
 }
