5a1d10ae17115c9ea45ba0428781b8eb9ba4340d
Renee Vandervelde <Renee@ReneeVandervelde.com>
2022-12-11 10:06:18 -0600

    Fix whitespace

--- units/src/commonMain/kotlin/inkapplications/spondee/measure/Irradiance.kt
+++ units/src/commonMain/kotlin/inkapplications/spondee/measure/Irradiance.kt
@@ -2,6 +2,7 @@ package inkapplications.spondee.measure
 
 import inkapplications.spondee.structure.Ratio
 import inkapplications.spondee.structure.Square
+
 /**
  * Unit of power density by area.
  */
