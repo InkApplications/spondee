d44ef707f35a7887309b6e30c1112f3a8ff0d87b
Renee Vandervelde <Renee@ReneeVandervelde.com>
2024-02-10 17:14:21 -0600

    Set up version catalog

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -7,7 +7,7 @@ repositories {
 }
 
 dependencies {
-    implementation(kotlin("gradle-plugin", "1.9.21"))
-    implementation("org.jetbrains.kotlinx:binary-compatibility-validator:0.13.2")
-    implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.9.10")
+    implementation(libs.kotlin.gradle)
+    implementation(libs.dokka)
+    implementation(libs.kotlinx.binary.compatibility)
 }
